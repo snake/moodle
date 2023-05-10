c61db9da549cceda028031bf2c836cf4fac669db
Jake Dallimore <jake@moodle.com>
2023-05-10 10:02:38 +0800

    MDL-76842 auth_lti: remove redundant conditional logic and cast

--- auth/lti/auth.php
+++ auth/lti/auth.php
@@ -181,7 +181,7 @@ class auth_plugin_lti extends \auth_plugin_base {
         unset($member['picture']);
 
         if ($binduser = $this->get_user_binding($iss, $member['user_id'])) {
-            $user = \core_user::get_user((int) $binduser);
+            $user = \core_user::get_user($binduser);
             $this->update_user_account($user, $member, $iss);
             return \core_user::get_user($user->id);
         } else {
