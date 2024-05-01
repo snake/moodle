b4a0944b60bb678781fff7b186db2a7947c709bb
Dan Marsden <dan@danmarsden.com>
2024-05-01 14:58:05 +1200

    MDL-79595 core_ltix: promote ltiservice profile

--- public/ltix/service/profile/classes/privacy/provider.php
+++ public/ltix/service/profile/classes/privacy/provider.php
@@ -15,18 +15,32 @@
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
 /**
- * Version information for the ltiservice_profile service.
+ * Privacy Subsystem implementation for ltixservice_profile.
  *
- * @package    ltiservice_profile
- * @copyright  2014 Vital Source Technologies http://vitalsource.com
- * @author     Stephen Vickers
+ * @package    ltixservice_profile
+ * @copyright  2018 Mark Nelson <markn@moodle.com>
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
+namespace ltixservice_profile\privacy;
 
 defined('MOODLE_INTERNAL') || die();
 
+/**
+ * Privacy Subsystem for ltixservice_profile implementing null_provider.
+ *
+ * @copyright  2018 Mark Nelson <markn@moodle.com>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+class provider implements \core_privacy\local\metadata\null_provider {
 
-$plugin->version   = 2025041400;
-$plugin->requires  = 2025040800;
-$plugin->component = 'ltiservice_profile';
+    /**
+     * Get the language string identifier with the component's language
+     * file to explain why this plugin stores no data.
+     *
+     * @return string
+     */
+    public static function get_reason(): string {
+        return 'privacy:metadata';
+    }
+}
