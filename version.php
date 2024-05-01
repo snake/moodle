5657b0c3b5daf4f9d775a032372a95c6dae67b00
Dan Marsden <dan@danmarsden.com>
2024-05-01 15:10:00 +1200

    MDL-79593 core: rename gradebookservices table to permit migration
    This is the first of two steps to permit data migration from the
    ltiservice_gradebookservice plugin, which will be removed. This change
    lets the table exist beyond plugin uninstallation, at which point it
    can be used to migrate data during installation of the replacement
    plugin, ltixservice_gradebookservice.

--- version.php
+++ version.php
@@ -29,7 +29,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$version  = 2024100700.01;              // YYYYMMDD      = weekly release date of this DEV branch.
+$version  = 2024100700.02;              // YYYYMMDD      = weekly release date of this DEV branch.
                                         //         RR    = release increments - 00 in DEV branches.
                                         //           .XX = incremental changes.
 $release  = '5.0dev (Build: 20241011)';    // Human-friendly version name
