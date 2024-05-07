6dfea2818279cb64143b154fb409110e179805ef
Jake Dallimore <jake@moodle.com>
2024-05-07 14:31:28 +0800

    MDL-79845 core_ltix: add capability to view course external tools
    This replaces 'mod/lti:addpreconfiguredinstance' in the course tools
    view. This commit also ensures users having the old capability get the
    new one.

--- lib/db/access.php
+++ lib/db/access.php
@@ -2844,4 +2844,14 @@ $capabilities = array(
         'captype' => 'write',
         'contextlevel' => CONTEXT_MODULE
     ],
+
+    // Controls users ability to view the course tools.
+    'moodle/ltix:viewcoursetools' => [
+        'captype' => 'read',
+        'contextlevel' => CONTEXT_COURSE,
+        'archetypes' => [
+            'editingteacher' => CAP_ALLOW,
+            'manager' => CAP_ALLOW
+        ]
+    ],
 );
