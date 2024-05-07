27fb82433d047d833e20c70ebf305d2099932ee9
Jake Dallimore <jake@moodle.com>
2024-05-07 14:31:28 +0800

    MDL-79845 core_ltix: add capability to view course external tools
    This replaces 'mod/lti:addpreconfiguredinstance' in the course tools
    view. This commit also ensures users having the old capability get the
    new one.

--- course/lib.php
+++ course/lib.php
@@ -3689,6 +3689,7 @@ function course_get_user_navigation_options($context, $course = null) {
         'tags' => false,
         'communication' => false,
         'overview' => false,
+        'ltix' => false,
     ];
 
     $options->blogs = !empty($CFG->enableblogs) &&
@@ -3773,6 +3774,9 @@ function course_get_user_navigation_options($context, $course = null) {
         $options->overview = has_capability('moodle/course:viewoverview', $context);
     }
 
+    if (has_capability('moodle/ltix:viewcoursetools', $context)) {
+        $options->ltix = true;
+    }
     return $options;
 }
 
