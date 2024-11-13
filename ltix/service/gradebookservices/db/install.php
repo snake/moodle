d8fe036d820a467600a388eeb679641c00765ac3
Jake Dallimore <jake@moodle.com>
2024-11-13 17:05:59 +0800

    MDL-83509 ltixservice_gradebookservices: update foreign key references
    The should now point to the lti_resource_link records which live inside
    core_ltix and have been populated from legacy data already.

--- ltix/service/gradebookservices/db/install.php
+++ ltix/service/gradebookservices/db/install.php
@@ -38,6 +38,37 @@ function xmldb_ltixservice_gradebookservices_install() {
                        FROM {tmp_ltiservice_gradebookservices}';
         $DB->execute($sql);
 
+        // Clean up orphaned records before fixing the references.
+        // Historically, this was handled with a task, so there could be a few pending removal since last task run (pre-upgrade).
+        // This is the same SQL that runs in gradebookservices::delete_orphan_ltixservice_gradebookservices_rows().
+        $sql = "DELETE
+                  FROM {ltixservice_gradebookservices}
+                 WHERE gradeitemid NOT IN (SELECT id
+                                             FROM {grade_items} gi)";
+        $DB->execute($sql);
+
+        // Drop the FK to lti.id and recreate it with lti_resource_link.id.
+
+        // Define key ltilinkid (foreign) to be dropped form ltixservice_gradebookservices.
+        $table = new xmldb_table('ltixservice_gradebookservices');
+        $key = new xmldb_key('ltilinkid', XMLDB_KEY_FOREIGN, ['ltilinkid'], 'lti', ['id']);
+
+        // Launch drop key ltilinkid.
+        $dbman->drop_key($table, $key);
+
+        // Define key ltilinkid (foreign) to be added to ltixservice_gradebookservices.
+        $key = new xmldb_key('ltilinkid', XMLDB_KEY_FOREIGN, ['ltilinkid'], 'lti_resource_link', ['id']);
+
+        // Launch add key ltilinkid.
+        $dbman->add_key($table, $key);
+
+        // Update each entry to point to the resource link instead of the lti activity instance.
+        $sql = 'UPDATE {ltixservice_gradebookservices}
+                   SET ltilinkid = rl.id
+                  FROM {lti_resource_link} rl
+                 WHERE ltilinkid = rl.legacyid';
+        $DB->execute($sql);
+
         $dbman->drop_table(new xmldb_table('tmp_ltiservice_gradebookservices'));
     }
 }
