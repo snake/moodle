3495cfcb52b9034f1cdb092490337316e40c2a8f
Shamim Rezaie <shamim@moodle.com>
2024-11-18 15:17:58 +1100

    MDL-82597 ltixservice_basicoutcomes: AMOS script
    AMOS BEGIN
     MOV [allow,ltiservice_basicoutcomes],[allow,ltixservice_basicoutcomes]
     MOV [ltiservice_basicoutcomes,ltiservice_basicoutcomes],[ltixservice_basicoutcomes,ltixservice_basicoutcomes]
     MOV [ltiservice_basicoutcomes_help,ltiservice_basicoutcomes],[ltixservice_basicoutcomes_help,ltixservice_basicoutcomes]
     MOV [notallow,ltiservice_basicoutcomes],[notallow,ltixservice_basicoutcomes]
     MOV [pluginname,ltiservice_basicoutcomes],[pluginname,ltixservice_basicoutcomes]
     MOV [privacy:metadata:externalpurpose,ltiservice_basicoutcomes],[privacy:metadata:externalpurpose,ltixservice_basicoutcomes]
     MOV [privacy:metadata:grade,ltiservice_basicoutcomes],[privacy:metadata:grade,ltixservice_basicoutcomes]
     MOV [privacy:metadata:userid,ltiservice_basicoutcomes],[privacy:metadata:userid,ltixservice_basicoutcomes]
    AMOS END

--- public/ltix/service/basicoutcomes/version.php
+++ public/ltix/service/basicoutcomes/version.php
@@ -26,6 +26,6 @@
 defined('MOODLE_INTERNAL') || die();
 
 
-$plugin->version   = 2026042000;
+$plugin->version   = 2026061000;
 $plugin->requires  = 2026041000;
 $plugin->component = 'ltixservice_basicoutcomes';
