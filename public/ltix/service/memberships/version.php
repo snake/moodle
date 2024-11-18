089fdc4313d209535d7d82facf48f8d7f8aa0834
Shamim Rezaie <shamim@moodle.com>
2024-11-18 15:09:43 +1100

    MDL-82597 ltixservice_memberships: AMOS script
    AMOS BEGIN
     MOV [allow,ltiservice_memberships],[allow,ltixservice_memberships]
     MOV [ltiservice_memberships,ltiservice_memberships],[ltixservice_memberships,ltixservice_memberships]
     MOV [ltiservice_memberships_help,ltiservice_memberships],[ltixservice_memberships_help,ltixservice_memberships]
     MOV [notallow,ltiservice_memberships],[notallow,ltixservice_memberships]
     MOV [pluginname,ltiservice_memberships],[pluginname,ltixservice_memberships]
     MOV [privacy:metadata:email,ltiservice_memberships],[privacy:metadata:email,ltixservice_memberships]
     MOV [privacy:metadata:externalpurpose,ltiservice_memberships],[privacy:metadata:externalpurpose,ltixservice_memberships]
     MOV [privacy:metadata:firstname,ltiservice_memberships],[privacy:metadata:firstname,ltixservice_memberships]
     MOV [privacy:metadata:fullname,ltiservice_memberships],[privacy:metadata:fullname,ltixservice_memberships]
     MOV [privacy:metadata:lastname,ltiservice_memberships],[privacy:metadata:lastname,ltixservice_memberships]
     MOV [privacy:metadata:userid,ltiservice_memberships],[privacy:metadata:userid,ltixservice_memberships]
     MOV [privacy:metadata:useridnumber,ltiservice_memberships],[privacy:metadata:useridnumber,ltixservice_memberships]
    AMOS END

--- public/ltix/service/memberships/version.php
+++ public/ltix/service/memberships/version.php
@@ -27,6 +27,6 @@
 defined('MOODLE_INTERNAL') || die();
 
 
-$plugin->version   = 2025100600;
+$plugin->version   = 2025110600;
 $plugin->requires  = 2025092600;
 $plugin->component = 'ltixservice_memberships';
