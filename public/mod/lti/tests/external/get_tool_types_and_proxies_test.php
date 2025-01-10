7023139d0a09f4dd9390be83b81b06b6fe65d428
Jake Dallimore <jake@moodle.com>
2025-01-10 13:54:19 +0800

    MDL-79109 core_ltix: fix non-final classes added to ltix
    These were missed during a rebase.

--- public/mod/lti/tests/external/get_tool_types_and_proxies_test.php
+++ public/mod/lti/tests/external/get_tool_types_and_proxies_test.php
@@ -34,7 +34,7 @@ require_once($CFG->dirroot . '/webservice/tests/helpers.php');
  * @copyright  2021 Catalyst IT
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
-class get_tool_types_and_proxies_test extends externallib_advanced_testcase {
+final class get_tool_types_and_proxies_test extends externallib_advanced_testcase {
 
     /**
      * This method runs before every test.
