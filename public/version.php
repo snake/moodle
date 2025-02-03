6cfb4b572cc91135cd1511df4ad6f8900fd818c0
Jake Dallimore <jake@moodle.com>
2025-02-03 10:48:07 +0800

    MDL-84415 core_ltix: DB changes supporting basic tool placements

--- public/version.php
+++ public/version.php
@@ -29,7 +29,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$version  = 2026060500.03;              // YYYYMMDD      = weekly release date of this DEV branch.
+$version  = 2026060500.04;              // YYYYMMDD      = weekly release date of this DEV branch.
                                         //         RR    = release increments - 00 in DEV branches.
                                         //           .XX = incremental changes.
 $release  = '5.3dev (Build: 20260605)';    // Human-friendly version name
