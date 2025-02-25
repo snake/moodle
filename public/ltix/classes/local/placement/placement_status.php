1d03b8d4027625a6764e16f9761ceea46e9d6139
Jake Dallimore <jake@moodle.com>
2025-02-25 12:46:59 +0800

    MDL-84415 core_ltix: add skeleton for lti placement handler API
    Includes:
    - Change to apis.json to allow 'lti' as second level namespace, allowing
    components to implement handlers under component/classes/lti/placement/.
    - First cut of placement_handler interface + base classes. This will be
    built out in future.
    - Move placements_helper to placements_manager and make singleton
    - Add handler instance registration mechanism to placements_manager
    - Add handler instance caching mechanism to placements_manager
    - Tests covering handler instance fetching, using mocked plugin type.

--- public/ltix/classes/local/placement/placement_status.php
+++ public/ltix/classes/local/placement/placement_status.php
@@ -14,32 +14,16 @@
 // You should have received a copy of the GNU General Public License
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
+namespace core_ltix\local\placement;
+
 /**
- * Mock LTI placement types for the mock plugin fake_fullfeatured.
+ * Status of a placement in a context.
  *
- * @package    core
+ * @package    core_ltix
  * @copyright  2025 Jake Dallimore <jrhdallimore@gmail.com>
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
-
-defined('MOODLE_INTERNAL') || die();
-
-$placementtypes = [
-    // Valid type string and handler.
-    'fake_fullfeatured:myfirstplacementtype' => [
-        'handler' => \fake_fullfeatured\lti\placement\myfirstplacementtype::class
-    ],
-
-    // No handler specified, which is also a valid configuration.
-    'fake_fullfeatured:anotherplacementtype' => [],
-
-    // Invalid type for a handler.
-    'fake_fullfeatured:thirdplacementtype' => [
-        'handler' => \stdClass::class,
-    ],
-
-    // Invalid format for the placement type string.
-    'fake/fullfeatured_invalidplacementtypestring' => [
-        'handler' => \fake_fullfeatured\lti\placement\myfirstplacementtype::class
-    ],
-];
+enum placement_status: int {
+    case DISABLED = 0;
+    case ENABLED = 1;
+}
