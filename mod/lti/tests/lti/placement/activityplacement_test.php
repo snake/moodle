7aab326eb8dd6a3da1faedde8e431069e70b4b9a
Jake Dallimore <jake@moodle.com>
2025-02-26 10:48:55 +0800

    MDL-84415 mod_lti: implement deep linking handler for activityplacement
    This is just an empty implementation for now, but will be added to as
    the API evolves.

---
+++ mod/lti/tests/lti/placement/activityplacement_test.php
@@ -0,0 +1,38 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+namespace mod_lti;
+
+/**
+ * Mod LTI activityplacement handler testing.
+ *
+ * @covers \mod_lti\lti\placement\activityplacement
+ * @package    mod_lti
+ * @copyright  Jake Dallimore <jrhdallimore@gmail.com>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+final class activityplacement_test extends \advanced_testcase {
+
+    /**
+     * Test confirming that an instance of the handler can be fetched when needed.
+     *
+     * @return void
+     */
+    public function test_get_instance(): void {
+        $instance = \core_ltix\local\placement\placements_helper::get_deeplinking_placement_instance('mod_lti:activityplacement');
+        $this->assertInstanceOf(\core_ltix\local\placement\deeplinking_placement_handler::class, $instance);
+    }
+}
