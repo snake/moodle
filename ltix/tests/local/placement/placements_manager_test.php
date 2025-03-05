fa6ba63b1ec35f574b3823dd25f698e79a8e0342
Jake Dallimore <jake@moodle.com>
2025-03-05 16:32:29 +0800

    MDL-84415 core_ltix: WIP commit containing some useful API methods
    This was really just code built to test some placements concepts during
    development, but the SQL is useful and will eventually be used, likely
    inside a repository for tool placements, or something like that. Just
    capturing this in a commit for when that time comes...

--- ltix/tests/local/placement/placements_manager_test.php
+++ ltix/tests/local/placement/placements_manager_test.php
@@ -17,6 +17,7 @@
 namespace core_ltix\local\placement;
 
 use core\tests\fake_plugins_test_trait;
+use core_ltix\constants;
 
 /**
  * Placements helper tests.
@@ -118,5 +119,208 @@ final class placements_manager_test extends \advanced_testcase {
         } catch (\Exception $e) {
             $this->assertEquals('codingerror', $e->errorcode);
         }
+    }
+
+    /**
+     * Test fetching all placements for a tool.
+     *
+     * @return void
+     */
+    public function test_get_tool_placements(): void {
+        $this->resetAfterTest();
+
+        /** @var \core_ltix_generator $ltigenerator */
+        $ltigenerator = $this->getDataGenerator()->get_plugin_generator('core_ltix');
+        $tool1id = $ltigenerator->create_tool_types([
+            'name' => 'Example tool',
+            'baseurl' => 'http://example.com/tool/1'
+        ]);
+        $tool2id = $ltigenerator->create_tool_types([
+            'name' => 'Example tool',
+            'baseurl' => 'http://example.com/tool/1'
+        ]);
+
+        // Two tools: one has two placements, the other has one.
+        $placementtype1 = $ltigenerator->create_placement_type('core_ltix', 'core_ltix:myplacement');
+        $placementtype2 = $ltigenerator->create_placement_type('fake_component', 'fake_component:anotherplacement');
+        $placementtype3 = $ltigenerator->create_placement_type('mod_foo', 'mod_foo:fooplacement');
+        $tool1placement1 = $ltigenerator->create_placement($tool1id, $placementtype1->id);
+        $tool1placement2 = $ltigenerator->create_placement($tool1id, $placementtype2->id);
+        $tool2placement1 = $ltigenerator->create_placement($tool2id, $placementtype3->id);
+
+        // Tool 1 placements.
+        $returnedtool1placements = placements_manager::get_tool_placements($tool1id);
+        $this->assertCount(2, $returnedtool1placements);
+
+        $returnedtool1placement1 = $returnedtool1placements[$tool1placement1->id];
+        $this->assertEquals($placementtype1->type, $returnedtool1placement1->placementtype);
+        $this->assertEquals($tool1id, $returnedtool1placement1->toolid);
+        $this->assertEquals($tool1placement1->id, $returnedtool1placement1->id);
+
+        $returnedtool1placement2 = $returnedtool1placements[$tool1placement2->id];
+        $this->assertEquals($placementtype2->type, $returnedtool1placement2->placementtype);
+        $this->assertEquals($tool1id, $returnedtool1placement2->toolid);
+        $this->assertEquals($tool1placement2->id, $returnedtool1placement2->id);
+
+        // Tool 2 placements.
+        $returnedtool2placements = placements_manager::get_tool_placements($tool2id);
+        $this->assertCount(1, $returnedtool2placements);
+
+        $returnedtool2placement1 = $returnedtool2placements[$tool2placement1->id];
+        $this->assertEquals($placementtype3->type, $returnedtool2placement1->placementtype);
+        $this->assertEquals($tool2id, $returnedtool2placement1->toolid);
+        $this->assertEquals($tool2placement1->id, $returnedtool2placement1->id);
+    }
+
+    /**
+     * Test get_placement_status().
+     * @return void
+     */
+    public function test_get_placement_status(): void {
+        $this->resetAfterTest();
+
+        /** @var \core_ltix_generator $ltigenerator */
+        $ltigenerator = $this->getDataGenerator()->get_plugin_generator('core_ltix');
+
+        $tool1id = $ltigenerator->create_tool_types([
+            'name' => 'Example tool',
+            'baseurl' => 'http://example.com/tool/1',
+        ]);
+        $tool2id = $ltigenerator->create_tool_types([
+            'name' => 'Example tool',
+            'baseurl' => 'http://example.com/tool/1',
+            'lti_coursevisible' => constants::LTI_COURSEVISIBLE_NO,
+        ]);
+
+        $placementtype1 = $ltigenerator->create_placement_type('core_ltix', 'core_ltix:myplacement');
+        $placementtype2 = $ltigenerator->create_placement_type('fake_component', 'fake_component:anotherplacement');
+        $placementtype3 = $ltigenerator->create_placement_type('fake_component', 'fake_component:placementtwo');
+        $placementtype4 = $ltigenerator->create_placement_type('mod_foo', 'mod_foo:fooplacement');
+
+        $placementconfig1 = [
+            'default_usage' => 'enabled',
+            'supports_deep_linking' => 1,
+            'deep_linking_url' => 'https://example.com/deep_link_launch',
+        ];
+        $placementconfig2 = [
+            'default_usage' => 'disabled',
+            'supports_deep_linking' => 0,
+        ];
+        $placementconfig3 = [
+            'default_usage' => 'disabled',
+            'supports_deep_linking' => 0,
+        ];
+        $placementconfig4 = [
+            'default_usage' => 'enabled',
+            'supports_deep_linking' => 0,
+        ];
+        $tool1placement1 = $ltigenerator->create_placement($tool1id, $placementtype1->id, $placementconfig1);
+        $tool1placement2 = $ltigenerator->create_placement($tool1id, $placementtype2->id, $placementconfig2);
+        $tool1placement3 = $ltigenerator->create_placement($tool1id, $placementtype3->id, $placementconfig3);
+        $tool2placement1 = $ltigenerator->create_placement($tool2id, $placementtype4->id, $placementconfig4);
+
+        // Set the placement status for placement 3 to ENABLED in the course context. This will override the config value.
+        $course = $this->getDataGenerator()->create_course();
+        $context = \core\context\course::instance($course->id);
+        $ltigenerator->create_placement_status_in_context($tool1placement3->id, placement_status::ENABLED, $context->id);
+
+        // Get an enabled status.
+        $this->assertEquals(
+            placement_status::ENABLED,
+            placements_manager::get_placement_status($tool1placement1->id, $context->id)
+        );
+
+        // Get a disabled status.
+        $this->assertEquals(
+            placement_status::DISABLED,
+            placements_manager::get_placement_status($tool1placement2->id, $context->id)
+        );
+
+        // Can get the status when status is set at the context level.
+        $this->assertEquals(
+            placement_status::ENABLED,
+            placements_manager::get_placement_status($tool1placement3->id, $context->id)
+        );
+
+        // Can get the placement status even for a hidden tool.
+        $this->assertEquals(
+            placement_status::ENABLED,
+            placements_manager::get_placement_status($tool2placement1->id, $context->id)
+        );
+    }
+
+    /**
+     * Test getting a list of enabled placement types in a context.
+     *
+     * @return void
+     */
+    public function test_get_enabled_placement_types_in_context(): void {
+        $this->resetAfterTest();
+
+        /** @var \core_ltix_generator $ltigenerator */
+        $ltigenerator = $this->getDataGenerator()->get_plugin_generator('core_ltix');
+
+        $tool1id = $ltigenerator->create_tool_types([
+            'name' => 'Example tool',
+            'baseurl' => 'http://example.com/tool/1',
+            'lti_coursevisible' => constants::LTI_COURSEVISIBLE_PRECONFIGURED,
+        ]);
+        $tool2id = $ltigenerator->create_tool_types([
+            'name' => 'Example tool 2',
+            'baseurl' => 'http://example.com/tool/2',
+            'lti_coursevisible' => constants::LTI_COURSEVISIBLE_PRECONFIGURED,
+        ]);
+        $tool3id = $ltigenerator->create_tool_types([
+            'name' => 'Example tool 3 - HIDDEN',
+            'baseurl' => 'http://example.com/tool/3',
+            'lti_coursevisible' => constants::LTI_COURSEVISIBLE_NO,
+        ]);
+
+        // Create a couple of placements with associated config.
+        $placementtype1 = $ltigenerator->create_placement_type('core_ltix', 'core_ltix:myplacement');
+        $placementtype2 = $ltigenerator->create_placement_type('fake_component', 'fake_component:anotherplacement');
+        $placementtype3 = $ltigenerator->create_placement_type('fake_component', 'fake_component:testinghidden');
+
+        $placementconfig1 = [
+            'default_usage' => 'enabled',
+            'supports_deep_linking' => 1,
+            'deep_linking_url' => 'https://example.com/deep_link_launch'
+        ];
+        $placementconfig2 = [
+            'default_usage' => 'disabled',
+            'supports_deep_linking' => 0,
+        ];
+        $placementconfig3 = [
+            'default_usage' => 'enabled',
+            'supports_deep_linking' => 0,
+        ];
+        $placementconfig4 = [
+            'default_usage' => 'enabled',
+            'supports_deep_linking' => 0,
+        ];
+        $tool1placement1 = $ltigenerator->create_placement($tool1id, $placementtype1->id, $placementconfig1);
+        $tool1placement2 = $ltigenerator->create_placement($tool1id, $placementtype2->id, $placementconfig2);
+        $tool2placement1 = $ltigenerator->create_placement($tool2id, $placementtype1->id, $placementconfig3);
+        $tool3placement1 = $ltigenerator->create_placement($tool3id, $placementtype3->id, $placementconfig4);
+
+        // Override the placement status for tool2placement1 in a context.
+        $course = $this->getDataGenerator()->create_course();
+        $context = \core\context\course::instance($course->id);
+        $ltigenerator->create_placement_status_in_context($tool2placement1->id, placement_status::DISABLED, $context->id);
+
+        // Despite 2 tool using $placementtype1, tool2 has a status of DISABLED in the context and is omitted.
+        $type1tools = placements_manager::get_enabled_placements_of_type_in_context($placementtype1->type, $context->id);
+        $this->assertCount(1, $type1tools);
+
+        // Tool 1's second placement has default_usage=disabled so, despite the tool being visible to courses, won't be included
+        // unless overridden at the course context.
+        $type2tools = placements_manager::get_enabled_placements_of_type_in_context($placementtype2->type, $context->id);
+        $this->assertCount(0, $type2tools);
+
+        // Verify that despite a placement config being present, any hidden tools are not listed.
+        $type3tools = placements_manager::get_enabled_placements_of_type_in_context($placementtype3->type, $context->id);
+        $this->assertCount(0, $type3tools);
+
+        // TODO: verify the specific data being returned...eventually moving to objects + repo.
     }
 }
