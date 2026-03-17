def8869a7fc3a337e161a97114f3999cd1de21f5
Jake Dallimore <jake@moodle.com>
2026-03-17 13:34:40 +0800

    MDL-83509 core_ltix: define mechanics of the parameter build pipeline
    Creates a pipeline which is composed of processors. During execution,
    each processor is called, in order, to build up an output array of
    parameters.

---
+++ public/ltix/classes/local/lticore/message/payload/parameters/pipeline/core/parameters_processor.php
@@ -0,0 +1,36 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+namespace core_ltix\local\lticore\message\payload\parameters\pipeline\core;
+
+use core_ltix\local\lticore\message\context\collection\launch_context;
+
+/**
+ * Defines behaviour for a parameters_processor.
+ *
+ * @internal
+ * @package    core_ltix
+ * @copyright  2026 Jake Dallimore <jrhdallimore@gmail.com>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+interface parameters_processor {
+    /**
+     * @param array $parameters the array of all parameters processed up to this point.
+     * @param launch_context $data runtime data available to be used by the processor.
+     * @return array the processed parameters, possibly modified.
+     */
+    public function process(array $parameters, launch_context $data): array;
+}
